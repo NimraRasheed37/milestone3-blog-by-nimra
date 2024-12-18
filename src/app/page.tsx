dabd93c7fce7b8f1d450c3da4bb715d13645653e
NimraRasheed37 <binteshaheen.na@gmail.com>
2024-12-18 20:31:55 +0500

    1st commit

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return( 
+  <>
+  <h1>Nimra's Blog</h1>
+  </>
+  );
+}
---
+++ src/components/BlogsCard.tsx
@@ -0,0 +1,9 @@
+
+export default function BlogsCard() {
+    return( 
+    <>
+    
+    </>
+    );
+  }
+  
