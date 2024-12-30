a0ae2845f3e8cef51abbc950be84d532071c65eb
NimraRasheed37 <binteshaheen.na@gmail.com>
2024-12-31 02:35:57 +0500

    footer updated

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,10 +1,11 @@
 import ContactForm from "@/components/ContactForm"
+import Footer from "@/components/Footer"
 
 export default function Contact(){
     return(
         <>
         <ContactForm/>
-        <div>Contact</div>
+        <Footer/>
         </>
     )
 }
